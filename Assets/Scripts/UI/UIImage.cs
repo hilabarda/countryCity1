92a57a33af3dbb7fd521fb730de3ab1508c49587
hila <hilabarda020@gmail.com>
2022-01-30 00:03:49 +0200

    Image

--- Assets/Scripts/UI/UIImage.cs
+++ Assets/Scripts/UI/UIImage.cs
@@ -6,20 +6,22 @@ using UnityEngine.UI;
 public class UIImage : MonoBehaviour
 {
     [SerializeField]
-    private Image[] imageCollectedP1;
+    private Image imageCollectedP1;
     [SerializeField]
     private Image[] imageCollectedP2;
     [SerializeField]
     private GameManager gameManager;
+    [SerializeField]
+    private Sprite sprite;
 
     private void Start()
     {
         gameManager.OnIconCollected += ChangeImage;
-        
+        imageCollectedP1 = GetComponent<Image>();
     }
 
     private void ChangeImage(int Icons)
     {
-        
+        imageCollectedP1.sprite = sprite;
     }
 }
