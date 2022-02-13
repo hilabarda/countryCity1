3197d6b99f8432c234e8b29f3cc40114e8d9d0c7
hila <hilabarda020@gmail.com>
2022-02-13 14:21:20 +0200

    Image

---
+++ Assets/Scripts/UI/MySprite.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class MySprite : MonoBehaviour
+{
+    [SerializeField]
+    public SpriteRenderer quad;
+
+
+    private void Start()
+    {
+        //quad = gameObject.GetComponent<SpriteRenderer>();
+
+    }
+
+ 
+    
+    
+}
